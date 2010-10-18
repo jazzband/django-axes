aa1bc1456baf50c7f1f8b6e1fa610ec2a122c5ae
Mike Blume <mike@loggly.com>
2010-10-18 03:31:59 +0000

    provide more fine-grained control over login patching

--- axes/middleware.py
+++ axes/middleware.py
@@ -12,3 +12,17 @@ class FailedLoginMiddleware(object):
 
         # and the regular auth login page
         auth_views.login = watch_login(auth_views.login)
+
+class FailedAdminLoginMiddleware(object):
+    def __init__(self, *args, **kwargs):
+        super(FailedAdminLoginMiddleware, self).__init__(*args, **kwargs)
+
+        # watch the admin login page
+        admin.site.login = watch_login(admin.site.login)
+
+class FailedAuthLoginMiddleware(object):
+    def __init__(self, *args, **kwargs):
+        super(FailedAuthLoginMiddleware, self).__init__(*args, **kwargs)
+
+        # watch the admin login page
+        auth_views.login = watch_login(auth_views.login)
