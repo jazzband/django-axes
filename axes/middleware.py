3166adb9a40013b26f54588415c525c30e7d856b
Camilo Nova <camilo.nova@gmail.com>
2013-03-16 17:42:03 -0500

    Removed duplicated watching for the login function, because admin uses the login function from auth.views too. Fixes #17

--- axes/middleware.py
+++ axes/middleware.py
@@ -1,31 +1,11 @@
-from django.contrib import admin
 from django.contrib.auth import views as auth_views
+
 from axes.decorators import watch_login
 
 
 class FailedLoginMiddleware(object):
-
     def __init__(self, *args, **kwargs):
         super(FailedLoginMiddleware, self).__init__(*args, **kwargs)
 
-        # watch the admin login page
-        admin.site.login = watch_login(admin.site.login)
-
-        # and the regular auth login page
-        auth_views.login = watch_login(auth_views.login)
-
-
-class FailedAdminLoginMiddleware(object):
-    def __init__(self, *args, **kwargs):
-        super(FailedAdminLoginMiddleware, self).__init__(*args, **kwargs)
-
-        # watch the admin login page
-        admin.site.login = watch_login(admin.site.login)
-
-
-class FailedAuthLoginMiddleware(object):
-    def __init__(self, *args, **kwargs):
-        super(FailedAuthLoginMiddleware, self).__init__(*args, **kwargs)
-
-        # watch the admin login page
+        # watch the auth login
         auth_views.login = watch_login(auth_views.login)
