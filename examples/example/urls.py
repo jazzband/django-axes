e819c700490fe1890664eb18e9d50242abb96585
Artur Barseghyan <artur.barseghyan@gmail.com>
2013-12-24 19:41:06 +0100

    removed trash from example urls

--- examples/example/urls.py
+++ examples/example/urls.py
@@ -4,21 +4,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # django-fobi URLs:
-    #url(r'^fobi/', include('fobi.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
-
-    # django-fobi public forms contrib app:
-    #url(r'^', include('fobi.contrib.apps.public_forms.urls')),
 )
 
 if settings.DEBUG:
