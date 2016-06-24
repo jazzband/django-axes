b300910dc18c87629ddca95b7ac1577b1e806451
Camilo Nova <camilo.nova@axiacore.com>
2016-06-24 09:46:55 -0500

    :fire: Removed example project

--- examples/test.sh
+++
@@ -1,4 +0,0 @@
-reset
-./uninstall.sh
-./install.sh
-python example/manage.py test axes --traceback
--- examples/uninstall.sh
+++
@@ -1,5 +0,0 @@
-pip uninstall django-axes -y
-rm build -rf
-rm dist -rf
-rm django_axes.egg-info -rf
-rm django-axes.egg-info -rf
