d5a5e46b2fbc9284213aef3ec45f0605b002b7b1
Aaron Redalen <aaron@revup.com>
2016-08-05 09:40:12 -0700

    Reset all attempts when ip not specified
    When no ip address positional arguments are specified, reset all attempts, as with reset() and per documentation.

--- axes/management/commands/axes_reset.py
+++ axes/management/commands/axes_reset.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         count = 0
-        if kwargs:
+        if kwargs and kwargs.get('ip'):
             for ip in kwargs['ip']:
                 count += reset(ip=ip)
         else:
