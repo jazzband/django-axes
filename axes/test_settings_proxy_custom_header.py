41877cdecd400231c7642e511ace194ecea8a76f
Aleksi Häkli <aleksi.hakli@vincit.fi>
2016-11-21 21:33:53 +0200

    Fix and add tests for IPv4 and IPv6 parsing
    This patch does not fix IPv6 parsing with ports

---
+++ axes/test_settings_proxy_custom_header.py
@@ -0,0 +1,3 @@
+from .test_settings_proxy import *
+
+AXES_REVERSE_PROXY_HEADER = 'HTTP_X_AXES_CUSTOM_HEADER'
--- runtests.py
+++ runtests.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import os
 import sys
 
@@ -6,10 +7,18 @@ import django
 from django.conf import settings
 from django.test.utils import get_runner
 
-if __name__ == "__main__":
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'axes.test_settings'
+
+def run_tests(settings_module, *modules):
+    os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
-    failures = test_runner.run_tests(["axes"])
+    failures = test_runner.run_tests(*modules)
     sys.exit(bool(failures))
+
+
+if __name__ == '__main__':
+    run_tests('axes.test_settings', [
+        'axes.tests.AccessAttemptTest',
+        'axes.tests.UtilsTest',
+    ])
