fb1163e7332b639d9f1a9cbd6030982d5334a923
Michał Bielawski <michal.bielawski@apperio.com>
2018-01-20 18:52:01 +0000

    Changed the way the new mixin works. Added tests.

--- axes/apps.py
+++ axes/apps.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import logging
+import warnings
 
 from django import apps
 
@@ -10,19 +11,26 @@ from .conf import settings
 log = logging.getLogger(settings.AXES_LOGGER)
 
 
+def patch_login_view():
+    warnings.warn(
+        'Automatic patching of LoginView using AXES_USE_LEGACY_LOGIN_VIEW_PATCH is deprecated. '
+        'use axes.views.AxesLoginView instead, or add axes.views.AxesLoginMixin to your custom login view.',
+        DeprecationWarning
+    )
+    from django.contrib.auth.views import LoginView
+    from django.utils.decorators import method_decorator
+
+    from .decorators import axes_dispatch, axes_form_invalid
+
+    LoginView.dispatch = method_decorator(axes_dispatch)(LoginView.dispatch)
+    LoginView.form_invalid = method_decorator(axes_form_invalid)(LoginView.form_invalid)
+
+
 class AppConfig(apps.AppConfig):
     name = 'axes'
 
     def ready(self):
         from . import signals    # NOQA: Load the signals module to bind the receivers
 
-        if settings.USE_LEGACY_LOGIN_VIEW_PATCH:
-            log.warning('Automatic patching of LoginView using AXES_USE_LEGACY_LOGIN_VIEW_PATCH is deprecated. '
-                        'use axes.views.AxesLoginView instead, or add axes.views.AxesLoginMixin to your custom login view.')
-            from django.contrib.auth.views import LoginView
-            from django.utils.decorators import method_decorator
-
-            from .decorators import axes_dispatch, axes_form_invalid
-
-            LoginView.dispatch = method_decorator(axes_dispatch)(LoginView.dispatch)
-            LoginView.form_invalid = method_decorator(axes_form_invalid)(LoginView.form_invalid)
+        if settings.AXES_USE_LEGACY_LOGIN_VIEW_PATCH:
+            patch_login_view()
--- axes/views.py
+++ axes/views.py
@@ -1,18 +1,22 @@
 from __future__ import absolute_import
 
 from django.contrib.auth.views import LoginView
-from django.utils.decorators import method_decorator
 
-from .decorators import axes_dispatch, axes_form_invalid
+from axes.attempts import is_already_locked
+from .decorators import lockout_response
 
 
 class AxesLoginMixin(object):
-    @method_decorator(axes_dispatch)
     def dispatch(self, request, *args, **kwargs):
+        if is_already_locked(request):
+            return lockout_response(request)
+
         return super(AxesLoginMixin, self).dispatch(request, *args, **kwargs)
 
-    @method_decorator(axes_form_invalid)
     def form_invalid(self, form):
+        if is_already_locked(self.request):
+            return lockout_response(self.request)
+
         return super(AxesLoginMixin, self).form_invalid(form)
 
 
