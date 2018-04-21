69c97d4ceed63f2bab4308abe514cb495ef6c120
Aleksi Häkli <aleksi.hakli@iki.fi>
2018-04-21 16:55:17 +0300

    Ignore argument definitions and import reports for signal handlers and utilities
    Signed-off-by: Aleksi Häkli <aleksi.hakli@iki.fi>

--- axes/management/commands/axes_list_attempts.py
+++ axes/management/commands/axes_list_attempts.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     args = ''
     help = ('List registered login attempts')
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **kwargs):  # pylint: disable=unused-argument
         for obj in AccessAttempt.objects.all():
             self.stdout.write('{ip}\t{username}\t{failures}'.format(
                 ip=obj.ip_address,
