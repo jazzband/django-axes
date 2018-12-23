396d99ccc1c2569a63e51cbb8166c157e2c884b3
Aleksi Häkli <aleksi.hakli@iki.fi>
2018-12-23 15:44:51 +0100

    Fix tests for Django 2.1
    Use HTML comparisons is assertContains instead of
    normal text comparisons which can change with rendering.
    
    Fixes #374

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: MIT License',
