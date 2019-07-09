0f3b3527be4eeb2248238cee8dd7ef36aec2ed00
Hasan Ramezani <hasan.r67@gmail.com>
2019-07-09 12:14:15 +0200

    Remove unused imports.

--- axes/tests/test_login.py
+++ axes/tests/test_login.py
@@ -12,7 +12,7 @@ from django.urls import reverse
 from django.contrib.auth import get_user_model, login, logout
 
 from axes.conf import settings
-from axes.models import AccessLog, AccessAttempt
+from axes.models import AccessAttempt
 from axes.tests.base import AxesTestCase
 
 
