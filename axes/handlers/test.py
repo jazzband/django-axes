5f2541dfc72997759004b9cf83e69eac44c33c92
Aleksi Häkli <aleksi.hakli@iki.fi>
2020-01-01 22:50:28 +0200

    Specify abstract methods with correct return types

--- axes/handlers/test.py
+++ axes/handlers/test.py
@@ -6,11 +6,14 @@ class AxesTestHandler(AxesHandler):  # pylint: disable=unused-argument
     Signal handler implementation that does nothing, ideal for a test suite.
     """
 
-    def is_allowed(self, request, credentials: dict = None) -> bool:
-        return True
- 
     def reset_attempts(self, *, ip_address: str = None, username: str = None) -> int:
-        pass
+        return 0
 
     def reset_logs(self, *, age_days: int = None) -> int:
-        pass
+        return 0
+
+    def is_allowed(self, request, credentials: dict = None) -> bool:
+        return True
+
+    def get_failures(self, request, credentials: dict = None) -> int:
+        return 0
