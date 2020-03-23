344a97c694524fc37ecf827f34a7ddc7d747f54f
Aleksi Häkli <aleksi.hakli@iki.fi>
2020-03-23 13:58:12 +0200

    Fix code formatting for linter

--- axes/handlers/database.py
+++ axes/handlers/database.py
@@ -95,8 +95,8 @@ class AxesDatabaseHandler(AxesHandler):  # pylint: disable=too-many-locals
         )
 
         # This replaces null byte chars that crash saving failures, meaning an attacker doesn't get locked out.
-        get_data = get_query_str(request.GET).replace('\0', '0x00')
-        post_data = get_query_str(request.POST).replace('\0', '0x00')
+        get_data = get_query_str(request.GET).replace("\0", "0x00")
+        post_data = get_query_str(request.POST).replace("\0", "0x00")
 
         if self.is_whitelisted(request, credentials):
             log.info("AXES: Login failed from whitelisted client %s.", client_str)
