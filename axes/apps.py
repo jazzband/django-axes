aae0e295ef1c020371831aa9145820d8678670f7
Aleksi Häkli <aleksi.hakli@iki.fi>
2020-09-27 16:38:14 +0300

    Add QA skip for import ordering

--- axes/apps.py
+++ axes/apps.py
@@ -24,7 +24,7 @@ class AppConfig(apps.AppConfig):
         cls.initialized = True
         
         # Only import settings, checks, and signals one time after Django has been initialized
-        from axes.conf import settings
+        from axes.conf import settings  # noqa
         from axes import checks, signals  # noqa
 
         # Skip startup log messages if Axes is not set to verbose
