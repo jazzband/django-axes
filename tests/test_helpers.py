1a751d083da39e6ea6dbc16f8bf95153486ec43b
phil-bell <philhabell@gmail.com>
2021-08-17 16:17:24 +0100

    adding test for different http response code

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -677,6 +677,11 @@ class AxesLockoutTestCase(AxesTestCase):
         response = get_lockout_response(self.request, self.credentials)
         self.assertEqual(403, response.status_code)
 
+    @override_settings(AXES_HTTP_RESPONSE_CODE=429)
+    def test_get_lockout_response_with_custom_http_response_code(self):
+        response = get_lockout_response(self.request, self.credentials)
+        self.assertEqual(429, response.status_code)
+
     @override_settings(AXES_LOCKOUT_CALLABLE=mock_get_lockout_response)
     def test_get_lockout_response_override_callable(self):
         response = get_lockout_response(self.request, self.credentials)
