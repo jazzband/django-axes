81695f46c2547ab93500b53f05fef102057c1799
Hasan Ramezani <hasan.r67@gmail.com>
2022-11-15 13:09:30 +0100

    Fix black formatting

--- axes/__init__.py
+++ axes/__init__.py
@@ -3,6 +3,6 @@ try:
 except ImportError:
     from pkg_resources import get_distribution  # from setuptools, deprecated
 
-    __version__ = get_distribution('django-axes').version
+    __version__ = get_distribution("django-axes").version
 else:
-    __version__ = version('django-axes')
+    __version__ = version("django-axes")
